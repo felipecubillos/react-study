1a577b26b1e46b0bd6ef7127d8b43cfcdfbb964e
andres cubillos <felipe.cubillos@outlook.com>
2021-06-10 09:21:29 -0500

    new component created and linked in the main component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpensesItem from "./components/ExpensesItem";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is the principal component</h1>
+      <ExpensesItem></ExpensesItem>
     </div>
   );
 }
---
+++ src/components/ExpensesItem.js
@@ -0,0 +1,5 @@
+function ExpensesItem() {
+  return <h2>This is other componente, expenses component</h2>;
+}
+
+export default ExpensesItem;
